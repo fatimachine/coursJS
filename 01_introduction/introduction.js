7a8b1de1d02bfa6f25255a9e1cd579d4349ab939
Fatima CHINE <chinefatimabeya@gmail.com>
2021-09-24 16:56:48 +0200

    cours bootstrap
    debut bootstrap

--- 01_introduction/introduction.js
+++ 01_introduction/introduction.js
@@ -1,19 +1,24 @@
-// mon premier fichier JS 
+// Mon premier fichier .js
 
 // alert('coucou');
+
+/* un commentaire JS sur 
+plusieurs lignes */
 
 let bonjour = document.getElementById('b1');
 let ajouter = document.getElementById('b2');
 
-bonjour.addEventListener('click',popup);
-ajouter.addEventListener('click',ajout);
+bonjour.addEventListener('click', popup);
+ajouter.addEventListener('click', ajout);
 
 function popup(){
-    alert ('hé patrick vous avez un chargeur?');
+    alert('hé Patrick vous avez un chargeur ?');
 }
 
 function ajout(){
     let para = document.createElement('p');
+    // console.log(para);
     para.textContent = 'Paragraphe ajouté';
+    // console.log(para);
     document.body.appendChild(para);
 }
