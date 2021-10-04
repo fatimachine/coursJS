cc3451a944e2e72624517e767064ddb63ddb9b73
Fatima CHINE <chinefatimabeya@gmail.com>
2021-10-04 15:46:43 +0200

    coursJS
    coursJS 04/10/21

--- 02_variable_constante/concatenation.js
+++ 02_variable_constante/concatenation.js
@@ -1,1 +1,49 @@
 // Page JS pour la concaténation et les littéraux de gabarit 
+
+// let monAge = 19 + 1;
+// console.log(monAge);
+
+// utilisation de caractère d'échappement 
+// let phrase = 'Bonjour j\'ai';
+
+// console.log(phrase);
+
+// let phrase2 = "Bonjour j\'ai faim";
+// let test = 4+5;
+
+// console.log(monAge, phrase);
+
+// MINI EXO faire une alerte et mettre dans l'alerte "Bonjour j'ai 20 ans";
+
+
+// alert(phrase + ' '+monAge+' ans.');
+// console.log(phrase,monAge+' ans');
+
+let x = 501;
+let y = 99;
+
+// mini exo 
+
+// let somme = x +y;
+// console.log(a);
+
+// console.log(x,y);
+// console.log(x+y)
+
+// RESULTAT 
+// alert(' x contient ' + x + ' y contient ' + y + ' leur somme est égale à ' + (x+y)) ;
+// console.log(' x contient ' + x + ' y contient ' + y + ' leur somme est égale à ' + (x+y)) ;
+
+
+// en littéraux de gabarits avec l'accent grave 
+alert(`x contient ${x}
+y contient ${y}
+leur somme est égale à ${x + y}`);
+
+console.log(`x contient ${x}
+y contient ${y}
+leur somme est égale à ${x + y}`);
+
+
+
+
