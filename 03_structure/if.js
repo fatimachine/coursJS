0327d89201e9768da91b11a205aea9dddcecf8af
latifa chine <latifa.chine@colombbus.org>
2021-10-05 17:07:24 +0200

    coursJS modif 05/10
    modif apres-midi

---
+++ 03_structure/if.js
@@ -0,0 +1,16 @@
+// Js if 
+
+let x = 4;
+let y = 0;
+
+if(x > 1) {// if(TEST) si test est évalué à true 
+    alert(1/'x contient ' + x + ' et cette valeur est supérieure à 1');// on exécute ce bloc de code
+}
+
+if(y < 1 ) {
+    alert(2/'y contient '  + y + ' et cette valeur est inférieure à 1');
+}
+
+if(x == y ) {
+    alert(3/'x = '  + x +',et y ='+ y +'les deux variables contiennent la même valeur');
+}
