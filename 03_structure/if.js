881d10025aa840921102d96b109c8521d50551e8
Fatima CHINE <chinefatimabeya@gmail.com>
2021-10-07 15:17:56 +0200

    coursJS 07/10/21
    coursJS 07/10/21

--- 03_structure/if.js
+++ 03_structure/if.js
@@ -1,16 +1,35 @@
-// Js if 
-
 let x = 4;
-let y = 0;
+ let y =0;
+ if (x > 1) {
+     alert ('x contient  ' + x +  '  et cette valeur est supérieure à 1'  ) ;
+ }
+ if (y<1) {
+    alert ( ' y contient ' + y +  '  et cette valeur est inférieure');
+ }
+ if (x==y) {
+     alert('x=' + x +' et , y = ' + y + '  centiennent la même valeur ');
+ }
 
-if(x > 1) {// if(TEST) si test est évalué à true 
-    alert(1/'x contient ' + x + ' et cette valeur est supérieure à 1');// on exécute ce bloc de code
+//  l'alert ne s'affiche pas car la valeur  de y  qui contient 0 ,0 est une des valeurs qui renvoie false
+ if (y) {
+     alert (' la valeur de y évaluée à "true"');
+ }
+ 
+//5
+
+let chaine = '';
+if (chaine) {
+    alert ('5/true /// vrai');
 }
 
-if(y < 1 ) {
-    alert(2/'y contient '  + y + ' et cette valeur est inférieure à 1');
+//6 au lieu de vérifier que le test est vrai on va vérifier qu'il est faux
+if ((x>1) == false){
+    //si le résultat du test est faux...((false)==false);
+    // le code s'exécute
+    alert('6/ x  qui contient ' + x +' est inférieur à 1');
 }
 
-if(x == y ) {
-    alert(3/'x = '  + x +',et y ='+ y +'les deux variables contiennent la même valeur');
+//7
+if((x == y)== false){
+    alert('7/ x et y ne contiennent pas la même valeur ');
 }
---
+++ 03_structure/ifelse.js
@@ -0,0 +1,17 @@
+// feuille JS if/else 
+
+let x = 0.5;
+
+if(x>1) {
+    // si le test est vrai j'exécute le bloc qui suit 
+    alert('x contient' + x+ 'et cette valeur est supérieur à 1');
+}else{
+    // sinon j'exécute celui-ci 
+    alert('x contient' + x+ 'et cette valeur est inférieur à 1');  
+}
+
+//si (il pleut) {
+    // je sors pas
+// }sinon {
+    // je sors 
+// }
