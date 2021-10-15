d53c20107d1be0fca9d98ed9c644f1c6355a56f7
Fatima CHINE <chinefatimabeya@gmail.com>
2021-10-15 16:40:11 +0200

    cours js 15/10
    modif dernier commit

--- 04_fonctions/conclusion.js
+++ 04_fonctions/conclusion.js
@@ -29,7 +29,27 @@
 let para1 = document.getElementById('p1');
 console.log(typeof para1, para1);
 
-para1.addEventListener('click',function() {
+para1.addEventListener('click',function() {//sur cette variable j'ajoute un écouteur d'évènement avec au clique une fonction anonyme qui s'exécute à la demande
     alert('Eh ! Tu as bien cliqué sur le paragraphe 1!');
 });
+
+let para2 = document.getElementById('p2');
+// console.log(typeof para2, para2);
+
+para2.addEventListener('dblclick',function() {
+    alert('Eh !là un double clique sur le paragraphe!!');
+});
+
+// fonction récursive
+function decompte(t) {
+    if (t>10) {
+        document.getElementById('p3').innerHTML += t + ' - ';
+        return decompte(t - 10);
+    }else {
+        return t;
+    }
+}//fin fonction
+
+decompte(1000);
+
 
