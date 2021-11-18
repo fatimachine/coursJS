acbb3ace50fb6ced853402f1dc9e2a25b2f76090
Fatima CHINE <chinefatimabeya@gmail.com>
2021-11-18 16:31:31 +0100

    courJs
    18/11

--- 00_exo/chine_exo17.js
+++ 00_exo/chine_exo17.js
@@ -1,24 +1,137 @@
 // Page Js pour Chine exo17
 
-var nbre = 1;
+// var nbre = 1;
 
-for (var nbre = 1; nbre <=100; nbre++) {
-    if ((nbre % 3 === 0) && (nbre % 5 === 0)) {
-        console.log('GralaBu'); 
+// for (var nbre = 1; nbre <=100; nbre++) {
+//     if ((nbre % 3 === 0) && (nbre % 5 === 0)) {
+//         console.log('GralaBu'); 
       
-    } else if (nbre % 3 === 0) {
-        console.log('Grala');
+//     } else if (nbre % 3 === 0) {
+//         console.log('Grala');
  
-    } else if (nbre % 5 === 0) {
-        console.log('Bu'); 
+//     } else if (nbre % 5 === 0) {
+//         console.log('Bu'); 
 
-    }  
-    else {
-        console.log(nbre);
+//     }  
+//     else {
+//         console.log(nbre);
        
-        } 
+//         } 
      
-    }
+//     }
 
  
+    //Correction Fizzbuzz/grabalu
+
+   
+// for (var i = 1; i <=100; i++) {//le nombre est divisible par 3 et 5
+//     if ((i % 3 === 0) && (i % 5 === 0)) {
+//         console.log('Fizzbuzz'); 
+      
+//     } else if (i % 3 === 0) {
+//         console.log('Fizz');
  
+//     } else if (i % 5 === 0) {
+//         console.log('Buzz'); 
+
+//     }  
+//     else {
+//         console.log(i);
+       
+//         } 
+     
+//     }
+
+ // for (var i = 1; i <=100; i++) {//le nombre est divisible par 3 et 5
+//     if (i % 15 === 0) {
+//         console.log('Toutgoudou'); 
+      
+//     } else if (i % 3 === 0) {
+//         console.log('Tout');
+ 
+//     } else if (i % 5 === 0) {
+//         console.log('Goudou'); 
+
+//     }  
+//     else {
+//         console.log(i);
+       
+//         } 
+     
+//     }
+    for (var i = 1; i <= 100; i++) {
+        var message =  ' ' ; //initialisation de la var message
+        if (i % 3 === 0) { //Si le modulo de 3 est egale a 0, il est divisible par 3
+            message +='Grala' ; // on affiche le mot
+            }
+        if (i % 5 === 0){ //Si le modulo de 5 est egale a 0, il est divisible par 5
+            message += 'Bu' ; //on affiche bu
+            }
+        if (message == ' '){ // et sinon on affiche le i
+            message =  i ; // en même temps la variable va concaténer Grala + Bu si le nombre est un multiple de 3 ET de de 5
+            }
+        console.log(message);
+    }//fin de for
+    // alert('coucou');
+
+console.log('**************** 1 ********************');
+
+for ( var i = 1; i <= 100; i++ ) {
+    // console.log(i);
+    if ( (i % 3 === 0) && (i % 5 === 0) ) {// le nombre est divisible par 3 et 5 
+        console.info('FizzBuzz');
+    } else if ( i % 3 === 0) {
+        console.info('Fizz');
+    } else if ( i % 5 === 0) {
+        console.info('Buzz');
+    } else {
+        console.info(i);
+    }
+}// fin for
+
+console.log('**************** 2 ********************');
+
+for ( var i = 1; i <= 100; i++ ) {
+    // console.log(i);
+    if ( i % 15 === 0 ) {// le nombre est divisible par 15 c'est à dire par 3 et 5  
+        console.info('Toutgoudou');
+    } else if ( i % 3 === 0) {
+        console.info('Tout');
+    } else if ( i % 5 === 0) {
+        console.info('Goudou');
+    } else {
+        console.info(i);
+    }
+}// fin for
+
+console.log('**************** 3 ********************');
+
+for ( var i = 1; i <= 100; i++ ) {
+   var message = '';
+   
+    if ( i % 3 === 0 ) {// le nombre est divisible par 3  
+        message += 'Grala';// on affiche le mot
+    }
+    if ( i % 5 === 0 ) {// le nombre est divisible par 5  
+        message += 'Bu';// on affiche bu
+    } 
+    if (message == '') {// sinon on affiche la valeur numérique de i 
+        message = i;  // en même temps la variable va concaténer Grala + Bu si le nombre est un multiple de 3 et 5  
+    }
+    console.log(message)
+}// fin for
+
+console.log('**************** voir dans la page ********************');
+
+for ( var i = 1; i <= 100; i++ ) {
+    // console.log(i);
+    if ( (i % 3 === 0) && (i % 5 === 0) ) {// le nombre est divisible par 3 et 5 
+        document.getElementById('p1').innerHTML += 'FizzBuzz<br>';
+    } else if ( i % 3 === 0) {
+        document.getElementById('p1').innerHTML += 'Fizz<br>';
+    } else if ( i % 5 === 0) {
+        document.getElementById('p1').innerHTML += 'Buzz<br>';
+    } else {
+        document.getElementById('p1').innerHTML += i + '<br>';
+    }
+}// fin for
---
+++ 09_dom/dom.js
@@ -0,0 +1,5 @@
+// Page js 09_dom
+
+var calculIMC = function(poids, taille) {
+    return poids / (taille * taille);
+    };
