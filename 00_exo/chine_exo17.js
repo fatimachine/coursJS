196afe80e5fceac9b783ae71fc86f966ee478dac
Fatima CHINE <chinefatimabeya@gmail.com>
2021-11-18 13:29:36 +0100

    Cours jS et exos
    Cours jS et exos

---
+++ 00_exo/chine_exo17.js
@@ -0,0 +1,24 @@
+// Page Js pour Chine exo17
+
+var nbre = 1;
+
+for (var nbre = 1; nbre <=100; nbre++) {
+    if ((nbre % 3 === 0) && (nbre % 5 === 0)) {
+        console.log('GralaBu'); 
+      
+    } else if (nbre % 3 === 0) {
+        console.log('Grala');
+ 
+    } else if (nbre % 5 === 0) {
+        console.log('Bu'); 
+
+    }  
+    else {
+        console.log(nbre);
+       
+        } 
+     
+    }
+
+ 
+ 
